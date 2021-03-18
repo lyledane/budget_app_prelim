f219821df3c0854daade6e142e7565813d75d236
marBeee <marvi.fetiluna.20@usjr.edu.ph>
2021-03-19 01:36:51 +0800

    category_item_screen
    Added initial commits to category Item main screen and initial for list view

--- lib/screens/category_item_screen/category_item_screen.dart
+++ lib/screens/category_item_screen/category_item_screen.dart
@@ -1,25 +1,14 @@
-import 'package:budgets_app/bloc/cubit/dark_mode_cubit/dark_mode_cubit.dart';
-import 'package:budgets_app/bloc/cubit/selected_range_cubit/selected_range_cubit.dart';
-import 'package:budgets_app/bloc/item_bloc/item_bloc.dart';
-import 'package:budgets_app/constants/style.dart';
-import 'package:budgets_app/screens/main_screen/widgets/category_item_header.dart';
-import 'package:budgets_app/screens/category_item_screen/widgets/item_list.dart';
-import 'package:budgets_app/screens/category_item_screen/widgets/item_radial_chart.dart';
-import 'package:budgets_app/services/database_service.dart';
-import 'package:budgets_app/services/text_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class CategoryItemScreen extends StatefulWidget {
-  final DatabaseService databaseService;
   final String categoryName;
   final int categoryId;
   final String categoryLimit;
 
   const CategoryItemScreen({
     Key key,
-    this.databaseService,
     this.categoryName,
     this.categoryId,
     this.categoryLimit,
@@ -30,8 +19,6 @@ class CategoryItemScreen extends StatefulWidget {
 }
 
 class _CategoryItemScreenState extends State<CategoryItemScreen> {
-  final TextFormatter textFormatter = TextFormatter();
-
   PickerDateRange initialSelectedRange;
   DateTime startDate;
   DateTime endDate;
@@ -40,42 +27,18 @@ class _CategoryItemScreenState extends State<CategoryItemScreen> {
 
   @override
   void initState() {
-    var srs = BlocProvider.of<SelectedRangeCubit>(context).state;
-
-    if (srs is SelectedRangeSaved) {
-      startDate = DateTime.parse(srs.startDate);
-      endDate = DateTime.parse(srs.endDate);
-
-      initialSelectedRange = PickerDateRange(startDate, endDate);
-    }
-
-    BlocProvider.of<ItemBloc>(context).add(GetItemsEvent(
-      databaseService: widget.databaseService,
-      categoryId: widget.categoryId,
-      startDate: initialSelectedRange.startDate.toString(),
-      endDate: initialSelectedRange.endDate.toString(),
-    ));
-
-    var dm = BlocProvider.of<DarkModeCubit>(context).state;
-    if (dm is DarkModeSuccess) {
-      darkMode = dm.isDarkModeEnabled;
-    }
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kColorTransparent,
       body: SafeArea(
         child: Container(
           width: double.infinity,
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: darkMode == true
-                  ? [kColorDarkModeBG, kColorDarkModeBG, kColorDarkModeBG]
-                  : [kColorViolet1, kColorViolet1, kColorViolet2],
+              colors: [],
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
             ),
@@ -86,35 +49,7 @@ class _CategoryItemScreenState extends State<CategoryItemScreen> {
                 padding: EdgeInsets.symmetric(horizontal: 27),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CategoryItemHeader(
-                      databaseService: widget.databaseService,
-                      categoryId: widget.categoryId,
-                      categoryLimit: textFormatter.removeMoneyFormat(
-                        widget.categoryLimit,
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    Text(
-                      widget.categoryName,
-                      style: kSubText.copyWith(
-                        color: kColorWhite,
-                        fontSize: 21,
-                      ),
-                    ),
-                    SizedBox(height: 18),
-                    ItemRadialChart(
-                      categoryAmountLimit: widget.categoryLimit,
-                      color: darkMode == true ? kColorWhite : kColorViolet1,
-                    ),
-                    SizedBox(height: 30),
-                  ],
-                ),
-              ),
-              ItemList(
-                databaseService: widget.databaseService,
-                categoryLimit: textFormatter.removeMoneyFormat(
-                  widget.categoryLimit,
+                  children: [],
                 ),
               ),
             ],
--- lib/screens/category_item_screen/widgets/item_listview.dart
+++ lib/screens/category_item_screen/widgets/item_listview.dart
@@ -1,26 +1,15 @@
-import 'package:budgets_app/bloc/item_bloc/item_bloc.dart';
-import 'package:budgets_app/constants/apps_router.dart';
-import 'package:budgets_app/constants/style.dart';
-import 'package:budgets_app/global_widgets/custom_icon_slide_action.dart';
-import 'package:budgets_app/models/item.dart';
-import 'package:budgets_app/services/database_service.dart';
-import 'package:budgets_app/services/text_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class ItemListView extends StatefulWidget {
-  final List<Item> items;
   final double categoryLimit;
-  final DatabaseService databaseService;
   final PickerDateRange initialSelectedRange;
 
   const ItemListView({
     Key key,
-    this.items,
     this.categoryLimit,
-    this.databaseService,
     this.initialSelectedRange,
   }) : super(key: key);
 
@@ -29,14 +18,11 @@ class ItemListView extends StatefulWidget {
 }
 
 class _ItemListViewState extends State<ItemListView> {
-  final TextFormatter textFormatter = TextFormatter();
-
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: ListView.builder(
         padding: EdgeInsets.all(7),
-        itemCount: widget.items.length,
         itemBuilder: (context, index) {
           return Slidable(
             key: Key('$index'),
@@ -44,104 +30,11 @@ class _ItemListViewState extends State<ItemListView> {
               key: Key('$index'),
             ),
             actionExtentRatio: 0.25,
-            secondaryActions: [
-              CustomIconSlideAction(
-                color: kColorIndigo,
-                buttonText: 'Edit',
-                icon: Icons.edit,
-                onTap: () => Navigator.pushNamed(
-                  context,
-                  AppRouter.addEditItemScreen,
-                  arguments: {
-                    'databaseService': widget.databaseService,
-                    'categoryId': widget.items[index].categoryId,
-                    'categoryLimit': widget.categoryLimit,
-                    'isEdit': true,
-                    'item': widget.items[index]
-                  },
-                ),
-              ),
-              CustomIconSlideAction(
-                color: kColorRed,
-                buttonText: 'Delete',
-                icon: Icons.delete,
-                onTap: () async {
-                  await showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text(
-                          'Delete ${widget.items[index].itemName}',
-                          style: kCategoryItemTitle.copyWith(
-                            color: kColorBlack,
-                          ),
-                        ),
-                        content: Text(
-                          'Are you sure you want to delete ${widget.items[index].itemName} from item list?',
-                          style: kCategoryItemTitle.copyWith(
-                            color: kColorBlack,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
-                          ),
-                        ),
-                        actions: [
-                          TextButton(
-                            child: Text(
-                              'Delete',
-                              style: kSubText.copyWith(color: kColorRed),
-                            ),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-
-                              BlocProvider.of<ItemBloc>(context).add(
-                                DeleteItemEvent(
-                                  databaseService: widget.databaseService,
-                                  categoryId: widget.items[index].categoryId,
-                                  itemId: widget.items[index].id,
-                                  startDate: widget
-                                      .initialSelectedRange.startDate
-                                      .toString(),
-                                  endDate: widget.initialSelectedRange.endDate
-                                      .toString(),
-                                ),
-                              );
-
-                              setState(() {
-                                widget.items.removeAt(index);
-                              });
-                            },
-                          ),
-                          TextButton(
-                            child: Text('Cancel', style: kSubText),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-              ),
-            ],
+            secondaryActions: [],
             child: ListTile(
-              title: Text(
-                widget.items[index].itemName,
-                style: kCategoryItemTitle,
-              ),
-              subtitle: Text(
-                textFormatter.parseDate(
-                  dateFromDB: widget.items[index].dateSpent,
-                  fromDatabase: true,
-                ),
-                style: kSubText,
-              ),
-              trailing: Text(
-                '- ₱ ${textFormatter.parseAmount(widget.items[index].itemAmount)}',
-                style: kCategoryPriceText.copyWith(
-                  color: kColorRed,
-                ),
-              ),
+              title: Text('Title text'),
+              subtitle: Text('subTitle text'),
+              trailing: Text('Trailing'),
             ),
           );
         },
