00faea7c4f145380c0cdb642630759e6d4ff0565
lyle <76196715+lyledane@users.noreply.github.com>
2021-03-20 09:50:06 +0700

    Updating apps_router for the new screens

--- lib/constants/apps_router.dart
+++ lib/constants/apps_router.dart
@@ -1,3 +1,5 @@
+import 'package:budget_app_prelimm/screens/add_edit_category_screen/add_edit_category_screen.dart';
+import 'package:budget_app_prelimm/screens/add_edit_item_screen/add_edit_item_screen.dart';
 import 'package:budget_app_prelimm/screens/category_item_screen/category_item_screen.dart';
 import 'package:budget_app_prelimm/screens/main_screen/main_screen.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +7,8 @@ import 'package:flutter/material.dart';
 class AppRouter {
   static const String mainScreen = '/mainScreen';
   static const String categoryItemScreen = '/categoryItemScreen';
+  static const String addEditCategorySCreen = '/addEditCategorySCreen';
+  static const String addEditItemScreen = '/addEditItemScreen';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     Map args = settings.arguments;
@@ -14,6 +18,14 @@ class AppRouter {
       case categoryItemScreen:
         return MaterialPageRoute(
           builder: (_) => CategoryItemScreen(),
+        );
+      case addEditItemScreen:
+        return MaterialPageRoute(
+          builder: (_) => AddEditCategoryScreen(),
+        );
+      case addEditItemScreen:
+        return MaterialPageRoute(
+          builder: (_) => AddEditItemScreen(),
         );
       default:
         return MaterialPageRoute(
