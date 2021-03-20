caee32e283efa58d45c117614c5794f1aeeecd4b
lyle <76196715+lyledane@users.noreply.github.com>
2021-03-20 12:13:17 +0700

    Updating Code on main_screen and main_screen_header dart files

--- lib/screens/main_screen/widgets/main_screen_header.dart
+++ lib/screens/main_screen/widgets/main_screen_header.dart
@@ -1,8 +1,20 @@
+import 'package:budget_app_prelimm/screens/main_screen/widgets/settings_button.dart';
 import 'package:flutter/material.dart';
 
 class MainScreenHeader extends StatelessWidget {
+  const MainScreenHeader({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Row();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SettingsButton(settingsOnPressed: () {
+          Scaffold.of(context).openDrawer();
+        }),
+      ],
+    );
   }
 }
